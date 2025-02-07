6b3f3f79788a0b6b8fff9d66f3e271ef61aa49fa
Geovani Gomes <geovanny.1gomes@gmail.com>
2025-02-07 10:45:57 -0300

    create http halders initial

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-
-	repository "github.com/GeovanniGomes/blacklist/internal/infrastructure/repossitory"
+	"github.com/GeovanniGomes/blacklist/internal/infrastructure/transport/http/routes"
+	"github.com/gin-gonic/gin"
+	//repository "github.com/GeovanniGomes/blacklist/internal/infrastructure/repossitory"
 )
 
 func main() {
-    addr := os.Getenv("REDIS_ADDR")
-	password := os.Getenv("REDIS_PASSWORD")
-	dbStr := os.Getenv("REDIS_DB")
-    
-    db, err := strconv.Atoi(dbStr)
-		if err != nil {
-			panic(fmt.Errorf("error converter REDIS_DB to int: %v", err))
-		}
-
-    redisService, err := repository.NewRedisService(addr, password, db)
-    if err != nil {
-        log.Fatalf("Erro ao iniciar o RedisService: %v", err)
-    }
-
-    ctx := context.Background()
-	mapData, err := redisService.GetCache(ctx, "10_aushaushasu")
-    if err!= nil {
-        log.Println(err)
-    }
-	fmt.Print(mapData["reason"])
+	r := gin.Default()
+	routes.SetupRoutes(r)
+	r.Run(":8000")
 }
