e9956d50f9c1aab070ef962e523807106dd83d68
Geovani Gomes <geovanny.1gomes@gmail.com>
2025-02-08 16:20:30 -0300

    remove middleware

--- cmd/start_http.go
+++ cmd/start_http.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"github.com/GeovanniGomes/blacklist/internal/infrastructure/transport/http"
 	"github.com/GeovanniGomes/blacklist/internal/infrastructure/transport/http/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func StartHTTP() {
 	r := gin.Default()
-	r.Use(http.RecoveryMiddleware())
-
 	routes.SetupRoutes(r)
 	r.Run(":8000")
 }
