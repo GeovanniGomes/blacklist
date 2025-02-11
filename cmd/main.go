f8ca57ffcd733679903686f21b45789ba462b7d4
Geovani Gomes <geovanny.1gomes@gmail.com>
2025-02-11 15:04:40 -0300

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/GeovanniGomes/blacklist/cmd/setup"
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/GeovanniGomes/blacklist/cmd/setup"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	cwd, _ := os.Getwd()
+	loadDir := filepath.Join(cwd, "..", ".env")
+
+	err := godotenv.Load(loadDir)
+	if err != nil {
+		log.Fatalf("Erro ao carregar .env: %v", err)
+	}
+
 	container := setup.InitContainer()
 	setup.StartQueueConsumers(*container)
 	setup.StartHTTP(*container)
--- cmd/setup/start_http.go
+++ cmd/setup/start_http.go
@@ -7,6 +7,7 @@ import (
 )
 
 func StartHTTP(container depedence_injector.ContainerInjection) {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	routes.SetupRoutes(r, container)
 	r.Run(":8000")
